a13d3da17e34afb25883f32340b98888a56ac99e
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-03-03 16:07:03 +0000

    Feat: Started XML main layout and Recycler view (#9) * Added dataBinding for MainActivity.kt

--- app/src/main/java/com/leedsbeckett/todo_application/MainActivity.kt
+++ app/src/main/java/com/leedsbeckett/todo_application/MainActivity.kt
@@ -2,10 +2,19 @@ package com.leedsbeckett.todo_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.leedsbeckett.todo_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Instantiating binding variable
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Initialising binding variable
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Setting up the layout view
+        val view = binding.root
+        setContentView(view)
     }
 }

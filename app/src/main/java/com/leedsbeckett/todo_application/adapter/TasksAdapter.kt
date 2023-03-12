b0cd44f96fbd13cc58d853e85d11d6a6465f3f53
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-03-12 16:53:04 +0000

    Feat: Defined task and implemented part of TaskAdapter(#10) - Overridden onCreateViewHolder method - Created XML task_item for layout of each item

--- app/src/main/java/com/leedsbeckett/todo_application/adapter/TasksAdapter.kt
+++ app/src/main/java/com/leedsbeckett/todo_application/adapter/TasksAdapter.kt
@@ -1,5 +1,40 @@
 package com.leedsbeckett.todo_application.adapter
 
-class TasksAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.leedsbeckett.todo_application.R
+import com.leedsbeckett.todo_application.model.Task
+
+class TasksAdapter(private val context: Context, private val data: List<Task>)
+    : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>(){
+
+    // View holder nested class to hold items
+    class TaskViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        // Fetching the view from the layout
+        val taskView: TextView = view.findViewById(R.id.task_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        // Inflating the list of tasks
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.tasks_item, parent, false)
+        // Returning the view holder with the adapter layout
+        return TaskViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    /***
+     * Method called by layout manager to replace content of an object in the list
+     */
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 }
---
+++ app/src/main/java/com/leedsbeckett/todo_application/model/Task.kt
@@ -0,0 +1,3 @@
+package com.leedsbeckett.todo_application.model
+
+data class Task(val id: Int, val isDone: Boolean, val name: String)

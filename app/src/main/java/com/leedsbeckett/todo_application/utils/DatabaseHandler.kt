c6774bb40cb0829b3f1ae93d3b95a9d48c516c06
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-03-15 10:48:08 +0000

    Feat: Added function to insert task into database (#12)

--- app/src/main/java/com/leedsbeckett/todo_application/utils/DatabaseHandler.kt
+++ app/src/main/java/com/leedsbeckett/todo_application/utils/DatabaseHandler.kt
@@ -1,8 +1,11 @@
 package com.leedsbeckett.todo_application.utils
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.leedsbeckett.todo_application.model.Task
+import java.sql.RowId
 
 private const val NAME =            "Tasks.db"
 private const val VERSION =         1
@@ -10,20 +13,52 @@ private const val COLUMN_ID =       "_id"
 private const val COLUMN_TASK =     "task"
 private const val COLUMN_STATUS =   "task_status"
 
-class DatabaseHandler(
-    context: Context?,
-    name: String = NAME,
-    version: Int = VERSION,
-    openParams: SQLiteDatabase.OpenParams
-) : SQLiteOpenHelper(context, name, version, openParams) {
+private const val SQL_CREATE_ENTRIES =
+    "CREATE TABLE $NAME (" +
+            "$COLUMN_ID INTEGER PRIMARY KEY," +
+            "$COLUMN_TASK TEXT," +
+            "$COLUMN_STATUS BOOLEAN)"
+
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $NAME"
+
+/**
+ * Handler for managing the database to add and remove data
+ * full documentation at [Documentation Link](https://developer.android.com/training/data-storage/sqlite)
+ */
+class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    // Instantiating database helper
+    private val dbHelper : DatabaseHandler = DatabaseHandler(context)
+
 
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+
+        db?.execSQL(SQL_CREATE_ENTRIES)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+        // Dropping the previous table
+        db?.execSQL(SQL_DELETE_ENTRIES)
+        // Creating new table
+        db?.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    /**
+     * Adding task into the database using its values
+     */
+    fun addTask(task: Task) {
+        // Set database into write mode
+        val db = dbHelper.writableDatabase
+
+        // New map of values with name and status as keys
+        val values = ContentValues().apply {
+            put(COLUMN_TASK, task.name)
+            put(COLUMN_STATUS, task.isDone)
+        }
+        // Inserting the new values
+        val newRowId = db?.insert(NAME, null, values)
     }
 
 
-}
+} // End of class

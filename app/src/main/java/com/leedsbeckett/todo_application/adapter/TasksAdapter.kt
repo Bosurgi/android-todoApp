c04b88e58c0314b2117590b729dcaeb9bcb09b02
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-04-04 16:32:20 +0100

    feat: Task ID stored and checkbox updates Status * Set CheckBox Listeners when checkbox ticked * It assigns the ID to the Task corresponding to its DB value

--- app/src/main/java/com/leedsbeckett/todo_application/adapter/TasksAdapter.kt
+++ app/src/main/java/com/leedsbeckett/todo_application/adapter/TasksAdapter.kt
@@ -1,17 +1,21 @@
 package com.leedsbeckett.todo_application.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.TextView
+import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.leedsbeckett.todo_application.R
 import com.leedsbeckett.todo_application.model.Task
+import com.leedsbeckett.todo_application.utils.DatabaseHandler
 
 class TasksAdapter(private val context: Context, private val data: List<Task>)
     : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>(){
+
 
     // View holder nested class to hold items
     class TaskViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
@@ -24,6 +28,7 @@ class TasksAdapter(private val context: Context, private val data: List<Task>)
         // Inflating the list of tasks represented by Checkbox
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.tasks_item, parent, false)
+
         // Returning the view holder with the adapter layout
         return TaskViewHolder(adapterLayout)
     }
@@ -39,21 +44,22 @@ class TasksAdapter(private val context: Context, private val data: List<Task>)
      * Method called by layout manager to replace content of an object in the list
      */
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+
+        val db = DatabaseHandler(this.context)
         // Fetching the item from the dataset - List of Task at specified index
         val item: Task = data[position]
 
-        if (holder.taskView.isChecked) {
-            item.status = 1
-        }
-        // Setting the name
+        holder.taskView.setOnCheckedChangeListener{ _: CompoundButton, isChecked: Boolean ->
+            if(isChecked) {
+                db.updateStatus(item.id, 1)
+                Toast.makeText(this.context, "Task Done", Toast.LENGTH_LONG).show()
+            }
+        }// Setting the name
         holder.taskView.text = item.name
+        Log.d("Adapter", "OnBind accessed")
 
-        if (item.status == 1) {
-            holder.taskView.isChecked = true
-        }
 
         // Setting the task if performed to tick the checkbox
 //        item.status = if (holder.taskView.isChecked) 1 else 0
     }
-
 }

03d7c03d799aa69723313e703036593b6cfe6464
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-04-14 18:14:48 +0100

    feat: implemented fragment and container

---
+++ app/src/main/java/com/leedsbeckett/todo_application/fragments/NewTaskFragment.kt
@@ -0,0 +1,12 @@
+package com.leedsbeckett.todo_application.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leedsbeckett.todo_application.R
+
+class NewTaskFragment : Fragment() {
+
+}
